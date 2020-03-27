f7f8d3602d2bf3b2547bd47348de46bdd00f1dc5
Julian Rektenwald <julian.rektenwald@xion.at>
2020-03-27 15:38:33 +0100

    GHeht noch immer niht

--- project/app/src/main/java/com/example/antidoping/entities/Medis.kt
+++ project/app/src/main/java/com/example/antidoping/entities/Medis.kt
@@ -1,22 +1,29 @@
 package com.example.antidoping.entities
 
+import android.os.Parcelable
 import androidx.annotation.NonNull
 import androidx.room.*
-import androidx.room.ForeignKey.CASCADE
+import kotlinx.android.parcel.Parcelize
 
-@Entity(tableName = "Medis",primaryKeys = arrayOf("Uid","TakingUid") , foreignKeys = [ ForeignKey(entity = Takings::class, parentColumns = arrayOf("Uid"), childColumns = arrayOf("TakingUid"))] )
+@Parcelize
+@Entity(
+    tableName = "Medis",
+    primaryKeys = ["Uid"],
+    foreignKeys = [
+        ForeignKey(entity = Takings::class, parentColumns = arrayOf("Uid"), childColumns = arrayOf("TakingUid"))],
+    indices = [Index("TakingUid")])
 data class Medis(
-    //@ForeignKey(entity = Takings::class, parentColumns = arrayOf("Uid"), childColumns = arrayOf("Uid"), onDelete = CASCADE)
-
+    @ColumnInfo(name = "Uid")
     @NonNull val Uid: Int,
 
     @ColumnInfo(name = "Name") var Name: String?,
 
-    @NonNull@ColumnInfo(name = "TakingUid") var TakingUid: String,
+    @ColumnInfo(name = "TakingUid")
+    var TakingUid: String?,
     @ColumnInfo(name = "AG") var AG: String?,
     @ColumnInfo(name = "EW") var EW: String?,
     @ColumnInfo(name = "NW") var NW: String?,
     @ColumnInfo(name = "InCompetition") var InCompetition: Int?,
     @ColumnInfo(name = "OutCompetition") var OutCompetition: Int?,
     @ColumnInfo(name = "Warning") var Warning: Int?
-)
+):Parcelable
