d1095028dc65818c1a44fba0a83d822e15fa90d3
patrikkarasek <pkarasek@student.tgm.ac.at>
2020-04-19 04:09:12 +0200

    MenüÄctivitys hinzugefügt! Weiterleitung beim Anklicken eines Menüpunktes

---
+++ project/app/src/main/java/com/example/antidoping/VerbotActivity.kt
@@ -0,0 +1,11 @@
+package com.example.antidoping
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VerbotActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.verbot_layout)
+    }
+}
